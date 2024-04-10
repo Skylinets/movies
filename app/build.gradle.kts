16071fb9878954a1cfea578a01af09df856e3474
John mitchel Hansen montecinos <john.hansen_ex@scotiabank.cl>
2024-04-10 16:39:05 -0400

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.skyline.moviescatalog"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.skyline.moviescatalog"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.kotlin.coroutine.android)
+    implementation(libs.kotlin.coroutine.core)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.retrofit)
+    implementation(libs.dagger.hilt)
+    implementation(libs.dagger.hilt.navigation)
+    implementation(libs.kapt)
+    implementation(libs.moshi)
+    implementation(libs.moshi.retrofit)
+    implementation(libs.hilt.viewmodel)
+    implementation(libs.navigation.compose)
+    implementation(libs.viewmodel.compose)
+    implementation(libs.viewmodel.savedstate)
+    implementation(libs.viewmodel.ktx)
+    implementation(libs.androidx.coil)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
