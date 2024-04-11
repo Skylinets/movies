d1800f271b9d9ff9dff4a446c39c68d20ec38289
John <john.hansen.m@gmail.com>
2024-04-11 11:49:08 -0400

    WIP inject dependencies

--- app/src/main/java/com/skyline/moviescatalog/di/NetworkModule.kt
+++ app/src/main/java/com/skyline/moviescatalog/di/NetworkModule.kt
@@ -1,4 +1,34 @@
 package com.skyline.moviescatalog.di
 
+import com.skyline.moviescatalog.data.network.ApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi
+        .Builder()
+        .run {
+            add(KotlinJsonAdapterFactory())
+            build()
+        }
+    @Provides
+    @Singleton
+    fun providesApiService(moshi : Moshi): ApiService =
+        Retrofit.Builder()
+            .run {
+                baseUrl(ApiService.BASE_URL)
+                addConverterFactory(MoshiConverterFactory.create(moshi))
+                build()
+            }.create(ApiService::class.java)
 }
